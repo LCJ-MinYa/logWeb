19f02a5f613c97f7d0d7d33384289700929c0645
MinYa <1049468118@qq.com>
2018-11-15 13:54:19 +0800

    配置

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,3 +1,6 @@
+var url = window.location.hostname;
+var productionUrl = 'http://' + url + ':8084/life';
+
 export default {
-	API: process.env.NODE_ENV == 'production' ? 'https://api.ziyiu.com/life' : 'http://127.0.0.1:8084/life',
+    API: process.env.NODE_ENV == 'production' ? productionUrl : 'http://127.0.0.1:8084/life',
 }
