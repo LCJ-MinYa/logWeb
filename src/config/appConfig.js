7a72e4c2e3b62127f4297111ac48a7a5e2594ea9
MinYa <1049468118@qq.com>
2018-11-23 14:56:29 +0800

    外网服务器

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,5 +1,6 @@
-var url = window.location.hostname;
-var productionUrl = 'http://' + url + ':8084/life';
+// var url = window.location.hostname;
+// var productionUrl = 'http://' + url + ':8084/life';
+var productionUrl = 'https://api.ziyiu.com/life';
 
 export default {
     API: process.env.NODE_ENV == 'production' ? productionUrl : 'http://127.0.0.1:8084/life',
